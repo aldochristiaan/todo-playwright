e033e6256294865331db8d33217ac8aa2195e804
Aldo Christian <aldoc@backbase.com>
2023-03-12 11:42:40 +0100

    Add more tests

--- tests/page-objects/todo-page.ts
+++ tests/page-objects/todo-page.ts
@@ -4,7 +4,12 @@ import { testConfig } from '../test-config';
 
 export class TodoPage extends BasePage {
   readonly toDoInputField: Locator;
+  readonly toDoEditField: Locator;
+  readonly toDoDeleteButton: Locator;
+  readonly toDoToggle: Locator;
   readonly toDoCount: Locator;
+  readonly toDoList: Locator;
+  readonly clearCompletedButton: Locator;
 
   constructor(page: Page) {
     super(page, {
@@ -13,7 +18,12 @@ export class TodoPage extends BasePage {
     });
 
     this.toDoInputField = page.getByPlaceholder('What needs to be done?');
+    this.toDoEditField = page.locator('.edit');
     this.toDoCount = page.locator('.todo-count > strong');
+    this.toDoList = page.locator('.view > label');
+    this.toDoDeleteButton = page.locator('.destroy');
+    this.toDoToggle = page.locator('.toggle');
+    this.clearCompletedButton = page.locator('.clear-completed');
   }
 
   async validateInputField() {
@@ -24,6 +34,34 @@ export class TodoPage extends BasePage {
     await this.toDoInputField.fill(text);
     await expect(this.toDoInputField).toHaveValue(text);
     await this.page.keyboard.press('Enter');
+  }
+
+  async editTodo(from: string, to: string) {
+    await this.page.dblclick('label:has-text("' + from + '")');
+    await expect(this.toDoEditField).toHaveValue(from);
+    await this.toDoEditField.clear();
+    await this.toDoEditField.fill(to);
+    await this.page.keyboard.press('Enter');
+  }
+
+  async markToDoAsComplete() {
+    await this.toDoToggle.click();
+    await expect(this.clearCompletedButton).toBeVisible();
+    await this.clearCompletedButton.click();
+  }
+
+  async deleteToDo() {
+    await this.page.hover('.view > label');
+    await this.toDoDeleteButton.click();
+  }
+
+  async checkTodo(text: string) {
+    await this.page.hover('.view > label');
+    await this.toDoDeleteButton.click();
+  }
+
+  async validateToDoList(count: number) {
+    await expect(this.toDoList).toHaveCount(count);
   }
 
   async validateTodoCount(text: string) {
--- tests/specs/todo-positive.spec.ts
+++ tests/specs/todo-positive.spec.ts
@@ -4,6 +4,30 @@ test.describe('cRUD Flow', async () => {
   test('[@Smoke] Create to-do', async ({ todoPage }) => {
     await todoPage.validateInputField();
     await todoPage.addToDo('Hehe');
+    await todoPage.validateToDoList(1);
     await todoPage.validateTodoCount('1');
+  });
+
+  test('[@Smoke] Update to-do', async ({ todoPage }) => {
+    await todoPage.validateInputField();
+    await todoPage.addToDo('Todo');
+    await todoPage.editTodo('Todo', 'Other Todo');
+    await todoPage.addToDo('Todo 2');
+    await todoPage.validateToDoList(2);
+    await todoPage.validateTodoCount('2');
+  });
+
+  test('[@Smoke] Delete to-do', async ({ todoPage }) => {
+    await todoPage.validateInputField();
+    await todoPage.addToDo('Todo');
+    await todoPage.deleteToDo();
+    await todoPage.validateToDoList(0);
+  });
+
+  test('[@Smoke] Clear completed to-do', async ({ todoPage }) => {
+    await todoPage.validateInputField();
+    await todoPage.addToDo('Todo');
+    await todoPage.markToDoAsComplete();
+    await todoPage.validateToDoList(0);
   });
 });
