9cb1ae3475e9e3ab657b62292c8d4d62f729ae85
Aldo Christian <aldoc@backbase.com>
2023-03-12 11:02:16 +0100

    Add one positive case

--- tests/page-objects/todo-page.ts
+++ tests/page-objects/todo-page.ts
@@ -4,6 +4,7 @@ import { testConfig } from '../test-config';
 
 export class TodoPage extends BasePage {
   readonly toDoInputField: Locator;
+  readonly toDoCount: Locator;
 
   constructor(page: Page) {
     super(page, {
@@ -12,9 +13,20 @@ export class TodoPage extends BasePage {
     });
 
     this.toDoInputField = page.getByPlaceholder('What needs to be done?');
+    this.toDoCount = page.locator('.todo-count > strong');
   }
 
   async validateInputField() {
     await expect(this.toDoInputField).toBeVisible();
+  }
+
+  async addToDo(text: string) {
+    await this.toDoInputField.fill(text);
+    await expect(this.toDoInputField).toHaveValue(text);
+    await this.page.keyboard.press('Enter');
+  }
+
+  async validateTodoCount(text: string) {
+    await expect(this.toDoCount).toHaveText(text);
   }
 }
--- tests/specs/todo-positive.spec.ts
+++ tests/specs/todo-positive.spec.ts
@@ -3,5 +3,7 @@ import { test } from '../page-objects/test-runner';
 test.describe('cRUD Flow', async () => {
   test('[@Smoke] Create to-do', async ({ todoPage }) => {
     await todoPage.validateInputField();
+    await todoPage.addToDo('Hehe');
+    await todoPage.validateTodoCount('1');
   });
 });
